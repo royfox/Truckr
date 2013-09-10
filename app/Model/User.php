ac182cbc48d1b7574336c490253c7c43cef62b71
Mike Cripps <mike.cripps@footballradar.com>
2013-09-10 10:41:58 +0100

    Fix STRICT warnings for PHP5.4

--- app/Model/User.php
+++ app/Model/User.php
@@ -17,7 +17,7 @@ class User extends AppModel {
         )
     );
 
-    public function beforeSave() {
+    public function beforeSave($options = array()) {
         if (isset($this->data[$this->alias]['password'])) {
             $this->data[$this->alias]['password'] = AuthComponent::password($this->data[$this->alias]['password']);
         }
